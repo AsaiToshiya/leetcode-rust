ca281548071076688fb66bcfd916b0e788424833
Asai Toshiya <to.asai.60@gmail.com>
2022-11-11 19:58:08 +0900

    two_sum.rs を更新する

--- two_sum.rs
+++ two_sum.rs
@@ -3,16 +3,11 @@ use std::collections::BTreeMap;
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut map = BTreeMap::new();
-        for (i, n) in nums.iter().enumerate() {
-            map.insert(target - n, i);
-        }
-        for (i, m) in nums.iter().enumerate() {
-            if map.get(&m) != None {
-                let j = *map.get(&m).unwrap();
-                if i != j {
-                    return vec![i as i32, j as i32];
-                }
+        for j in 0..nums.len() {
+            if let Some(i) = map.get(&nums[j]) {
+                return vec![*i as i32, j as i32];
             }
+            map.insert(target - nums[j], j);
         }
         panic!();
     }
