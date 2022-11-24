b98f632b2c1f36d68a93f65f84089c3916b5e01b
Asai Toshiya <to.asai.60@gmail.com>
2022-11-25 07:53:13 +0900

    u8 の代わりに String を使用する

--- zigzag_conversion.rs
+++ zigzag_conversion.rs
@@ -5,24 +5,23 @@ impl Solution {
         if num_rows <= 1 {
             return s;
         }
-        let bytes = s.as_bytes();
         let mut i = 0;
         let round_trip_length = num_rows + num_rows - 2;
         let mut row_index = 0;
         let mut rows = Vec::new();
         for i in 0..num_rows {
-            rows.push(Vec::new());
+            rows.push("".to_string());
         }
-        while i < bytes.len() {
+        while i < s.len() {
             for j in 0..round_trip_length {
-                if i >= bytes.len() {
+                if i >= s.len() {
                     break;
                 }
                 row_index = (j - cmp::max(0, j - (round_trip_length - j))) as usize;
-                rows[row_index].push(bytes[i]);
+                rows[row_index].push_str(&s[i..i + 1]);
                 i += 1;
             }
         }
-        rows.iter().map(|r| String::from_utf8(r.to_vec()).unwrap()).collect::<Vec<_>>().join("")
+        rows.join("")
     }
 }
