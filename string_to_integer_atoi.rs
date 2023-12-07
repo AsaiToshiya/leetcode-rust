6e451f3a4e321dfa9cad4e7ef7544c7753854f7c
Asai Toshiya <to.asai.60@gmail.com>
2023-12-07 12:42:06 +0900

    早期リターン (break) するようにする

--- string_to_integer_atoi.rs
+++ string_to_integer_atoi.rs
@@ -7,31 +7,33 @@ impl Solution {
             }
 
             if let '+' | '-' | '0'..='9' = chars[i] {
-                let mut end: usize = i;
-                for j in i + 1..chars.len() {
-                    if let '0'..='9' = chars[j] {
-                        end = j;
-                    } else {
-                        break;
-                    }
-                }
-                let num_str = &s[i..end + 1];
-                if let Ok(n) = &num_str.parse::<i32>() {
-                    return *n as i32;
-                }
-                if num_str == "+" {
-                    return 0;
-                }
-                if num_str == "-" {
-                    return 0;
-                }
-                if &s[i..i + 1] == "-" {
-                    return i32::MIN;
+            } else {
+                break;
+            }
+
+            let mut end: usize = i;
+            for j in i + 1..chars.len() {
+                if let '0'..='9' = chars[j] {
+                    end = j;
                 } else {
-                    return i32::MAX;
+                    break;
                 }
             }
-            break;
+            let num_str = &s[i..end + 1];
+            if let Ok(n) = &num_str.parse::<i32>() {
+                return *n as i32;
+            }
+            if num_str == "+" {
+                return 0;
+            }
+            if num_str == "-" {
+                return 0;
+            }
+            if &s[i..i + 1] == "-" {
+                return i32::MIN;
+            } else {
+                return i32::MAX;
+            }
         }
 
         return 0;
