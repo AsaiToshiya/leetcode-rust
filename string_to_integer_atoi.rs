8eec822703d467b3cd3f39500d529fa018a451f6
Asai Toshiya <to.asai.60@gmail.com>
2023-12-07 22:57:40 +0900

    Revert "早期リターン (break) するようにする"
    This reverts commit 6e451f3a4e321dfa9cad4e7ef7544c7753854f7c.

--- string_to_integer_atoi.rs
+++ string_to_integer_atoi.rs
@@ -7,33 +7,31 @@ impl Solution {
             }
 
             if let '+' | '-' | '0'..='9' = chars[i] {
-            } else {
-                break;
-            }
-
-            let mut end: usize = i;
-            for j in i + 1..chars.len() {
-                if let '0'..='9' = chars[j] {
-                    end = j;
+                let mut end: usize = i;
+                for j in i + 1..chars.len() {
+                    if let '0'..='9' = chars[j] {
+                        end = j;
+                    } else {
+                        break;
+                    }
+                }
+                let num_str = &s[i..end + 1];
+                if let Ok(n) = &num_str.parse::<i32>() {
+                    return *n as i32;
+                }
+                if num_str == "+" {
+                    return 0;
+                }
+                if num_str == "-" {
+                    return 0;
+                }
+                if &s[i..i + 1] == "-" {
+                    return i32::MIN;
                 } else {
-                    break;
+                    return i32::MAX;
                 }
             }
-            let num_str = &s[i..end + 1];
-            if let Ok(n) = &num_str.parse::<i32>() {
-                return *n as i32;
-            }
-            if num_str == "+" {
-                return 0;
-            }
-            if num_str == "-" {
-                return 0;
-            }
-            if &s[i..i + 1] == "-" {
-                return i32::MIN;
-            } else {
-                return i32::MAX;
-            }
+            break;
         }
 
         return 0;
