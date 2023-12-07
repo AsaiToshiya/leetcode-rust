64050ca98aa2fdd92d5c9f10423f9297f0ebf102
Asai Toshiya <to.asai.60@gmail.com>
2023-12-08 00:24:26 +0900

    if と if let の代わりに match を使用する

--- string_to_integer_atoi.rs
+++ string_to_integer_atoi.rs
@@ -2,36 +2,35 @@ impl Solution {
     pub fn my_atoi(s: String) -> i32 {
         let chars: Vec<char> = s.chars().collect();
         for i in 0..chars.len() {
-            if chars[i] == ' ' {
-                continue;
-            }
-
-            if let '+' | '-' | '0'..='9' = chars[i] {
-                let mut end: usize = i;
-                for j in i + 1..chars.len() {
-                    if let '0'..='9' = chars[j] {
-                        end = j;
+            match chars[i] {
+                ' ' => continue,
+                '+' | '-' | '0'..='9' => {
+                    let mut end: usize = i;
+                    for j in i + 1..chars.len() {
+                        if let '0'..='9' = chars[j] {
+                            end = j;
+                        } else {
+                            break;
+                        }
+                    }
+                    let num_str = &s[i..end + 1];
+                    if let Ok(n) = &num_str.parse::<i32>() {
+                        return *n as i32;
+                    }
+                    if num_str == "+" {
+                        return 0;
+                    }
+                    if num_str == "-" {
+                        return 0;
+                    }
+                    if &s[i..i + 1] == "-" {
+                        return i32::MIN;
                     } else {
-                        break;
+                        return i32::MAX;
                     }
                 }
-                let num_str = &s[i..end + 1];
-                if let Ok(n) = &num_str.parse::<i32>() {
-                    return *n as i32;
-                }
-                if num_str == "+" {
-                    return 0;
-                }
-                if num_str == "-" {
-                    return 0;
-                }
-                if &s[i..i + 1] == "-" {
-                    return i32::MIN;
-                } else {
-                    return i32::MAX;
-                }
+                _ => break,
             }
-            break;
         }
 
         return 0;
